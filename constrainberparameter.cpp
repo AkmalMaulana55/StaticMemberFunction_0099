8c7dc7ccb112927a1d956b517379bb9d09dcde28
niel55-stack <vylingsniel55@gmail.com>
2026-05-04 18:32:37 +0700

    int main

--- constrainberparameter.cpp
+++ constrainberparameter.cpp
@@ -8,3 +8,16 @@ class mahasiswa{
 
 };
 //definisi Construktor dg parameter
+
+mahasiswa::mahasiswa(int nim, string nama){
+    cout<< "Construktor Dengan Parameter Terpanggil"<< endl;
+    cout << "NIM :"<< nim << endl;
+    cout << "Nama : "<<nama<<endl;
+
+}
+int main(){
+    //memanggil Construktor dg parameter
+    mahasiswa mhs(12345, "Pascal");
+    return 0;
+
+}
