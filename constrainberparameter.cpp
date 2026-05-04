d3958dea0b320fa4939816745e309f6addbd095c
niel55-stack <vylingsniel55@gmail.com>
2026-05-04 18:32:18 +0700

    class mahasiswa constrainberparameter

---
+++ constrainberparameter.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+class mahasiswa{
+    public:
+    //Construktor dengan parameter
+    mahasiswa(int nim, string nama);
+
+};
+//definisi Construktor dg parameter
